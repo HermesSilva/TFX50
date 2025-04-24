53def98da2e2babcfd5ac95769d6156ad825bdef
Hermes Silva <hxpelo@gmail.com>
2025-04-23 21:04:54 -0300

    .

--- Core/TFX.Core/Model/XModule.cs
+++ Core/TFX.Core/Model/XModule.cs
@@ -1,13 +1,26 @@
+using System;
+using System.Collections.Generic;
+
 using Microsoft.Extensions.DependencyInjection;
 
 using TFX.Core.Interfaces;
+using TFX.Core.Model.APP;
 
 namespace TFX.Core.Model
 {
     public abstract class XModule : XIModule
     {
+        protected XModule()
+        {
+            Apps = new Dictionary<Guid, XAPPModel>();
+        }
         public virtual void Initialize(IServiceCollection pServices)
         {
+        }
+
+        public Dictionary<Guid, XAPPModel> Apps
+        {
+            get; 
         }
     }
 }
