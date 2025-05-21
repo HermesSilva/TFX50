6fa15d665ce86b4a10aa39a0145069c8fbf8dc3b
Hermes Silva <hxpelo@gmail.com>
2025-05-21 20:53:18 -0300

    Commit

--- Core/TFX.Core.UI/src/Reflection/XReflections.ts
+++ Core/TFX.Core.UI/src/Reflection/XReflections.ts
@@ -1,34 +1,46 @@
-
 const AutoInitMarker = Symbol("AutoInitClass")
+const CreatingInstances = new Set<Function>()
 
 function AutoInit<T extends new (...args: any[]) => any>(Actual: T): T
 {
-    let current = Actual.prototype;
+    let current = Actual.prototype
     while (current && current !== Object.prototype)
     {
         if (current.constructor && (current.constructor as any)[AutoInitMarker])
-            throw new Error(`The Class "${Actual.name}" cannot use @AutoInit because a base class is already decorated.`);
+            throw new Error(`The class "${Actual.name}" cannot use @AutoInit because a base class is already decorated.`)
 
-        current = Object.getPrototypeOf(current);
+        current = Object.getPrototypeOf(current)
     }
 
     const Derived = class extends Actual
     {
         constructor(...args: any[])
         {
-            super(...args)
-            XObjectCache.ResolveDependencies(this)
-        }
-    };
+            if (CreatingInstances.has(Actual))
+                throw new Error(`Circular dependency detected for class "${Actual.name}"`)
 
-    Object.defineProperty(Derived, AutoInitMarker, { value: true, enumerable: false, configurable: false, writable: false });
+            CreatingInstances.add(Actual)
+            try
+            {
+                super(...args)
+                XObjectCache.ResolveDependencies(this)
+            }
+            finally
+            {
+                CreatingInstances.delete(Actual)
+            }
+        }
+    }
+
+    Object.defineProperty(Derived, AutoInitMarker, { value: true, enumerable: false, configurable: false, writable: false })
+
     return Derived as T
 }
 
 interface XInjectionItem
 {
-    Token: Function;
-    Key: string;
+    Token: Function
+    Key: string
 }
 
 function GetClassHierarchy(obj: any): Function[]
@@ -38,16 +50,17 @@ function GetClassHierarchy(obj: any): Function[]
 
     while (current && current !== Object.prototype)
     {
-        if (current.constructor?.name)
-            hierarchy.push(current.constructor)
+        hierarchy.push(current.constructor)
         current = Object.getPrototypeOf(current)
     }
+
     return hierarchy
 }
 
 class XObjectCache
 {
     private static _Providers = new Map<Function, any>()
+    private static _Creating = new Set<Function>()
 
     static AddProvider(token: Function)
     {
@@ -56,34 +69,38 @@ class XObjectCache
 
     static Get<T>(token: new () => T): T
     {
+        if (XObjectCache._Creating.has(token))
+            throw new Error(`Circular resolution detected for class "${token.name}"`)
+
         let instance = XObjectCache._Providers.get(token)
         if (!instance)
         {
-            instance = new (token as any)()
-            XObjectCache._Providers.set(token, instance)
+            try
+            {
+                XObjectCache._Creating.add(token)
+                instance = new (token as any)()
+                XObjectCache._Providers.set(token, instance)
+            }
+            finally
+            {
+                XObjectCache._Creating.delete(token)
+            }
         }
         return instance as T
     }
 
-    static ResolveDependencies<T>(instance: any): void
+    static ResolveDependencies(instance: any): void
     {
-        let x = GetClassHierarchy(instance);
-        for (var i = 0; i < x.length; i++)
+        const classes = GetClassHierarchy(instance)
+        for (const cls of classes)
         {
-            const item = <any>x[i];
-            if (item == null)
-                continue;
-            if (item.prototype.__inject__)
+            const injects = cls.prototype?.__inject__ as XInjectionItem[] | undefined
+            if (!injects) continue
+
+            for (const item of injects)
             {
-                const injects = <XInjectionItem[]>item.prototype.__inject__;
-                for (var j = 0; j < injects.length; j++)
-                {
-                    const item = injects[j];
-                    let vlr = instance[item.Key];
-                    if (vlr)
-                        continue;
-                    instance[item.Key] = XObjectCache.Get(<any>item.Token)
-                }
+                if (instance[item.Key]) continue
+                instance[item.Key] = XObjectCache.Get(item.Token as any)
             }
         }
     }
@@ -94,10 +111,8 @@ function Inject(token: Function): PropertyDecorator
     return function (target: any, propertyKey: string | symbol): void
     {
         if (!target.__inject__)
-            target.__inject__ = [];
+            target.__inject__ = []
 
-        target.__inject__.push({ Token: token, Key: propertyKey });
-        if (!target.__name__)
-            target.__name__ = target.name;
+        target.__inject__.push({ Token: token, Key: propertyKey })
     }
 }
