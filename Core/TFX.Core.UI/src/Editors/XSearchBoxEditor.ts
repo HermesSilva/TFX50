c9a0a07e0498ad25a22d9c8be1bf65d78706d026
Hermes Silva <hxpelo@gmail.com>
2025-05-26 13:26:12 -0300

    Commit

--- Core/TFX.Core.UI/src/Editors/XSearchBoxEditor.ts
+++ Core/TFX.Core.UI/src/Editors/XSearchBoxEditor.ts
@@ -1,18 +1,33 @@
 ﻿/// <reference path="XStringEditor.ts" />
 
-class XSearchBoxEditor extends XStringEditor 
+class XSearchBoxEditor extends XBaseInput 
 {
     constructor(pOwner: XElement | HTMLElement | null)
     {
         super(pOwner);
-        this.Input.className = "XSearchBoxEditor";
         this.ELMTitle.HTML.innerHTML = "Pesquisa ";
         this.Button = new XSVGButton(this, "XSearchBoxEditorButton");
-        this.Button.SVG.className = "XSearchIcon";    
+        this.Button.SVG.className = "XSearchIcon";
         this.Button.SetIcon("svg/search.svg");
-        //this.Campo = new XDiv(this, "InputTitle");
+        for (var i = 0; i < 10; i++)
+        {
+            this.EditableTag = new XEditableTag(this.Input);
+            this.EditableTag.OnClick = (pTag: XEditableTag) => this.Close(pTag);
+        }
+
     }
     Button: XSVGButton;
+    EditableTag!: XEditableTag;
+
+    Close(pTag: XEditableTag)
+    {
+        pTag.Free();
+    }
+
+    CreateInput(): HTMLInputElement
+    {
+        return XUtils.AddElement<HTMLInputElement>(this.HTML, "div", "XSearchBoxEditor");
+    }
 
     get Title(): string
     {
@@ -20,6 +35,6 @@ class XSearchBoxEditor extends XStringEditor
     }
     set Title(pValue: string)
     {
-        
+
     }
 }
---
+++ Core/TFX.Core.UI/src/Elements/XEditableTag.ts
@@ -0,0 +1,73 @@
+﻿/// <reference path="Base/XElement.ts" />
+/// <reference path="../Reflection/XReflections.ts" />
+
+class XTagEditor extends XDiv
+{
+    constructor(pOwner: XElement | HTMLElement | null, pClass: string | null)
+    {
+        super(pOwner, pClass);
+        this.Title = XUtils.AddElement<HTMLSpanElement>(this.HTML, "span", "XTagEditorTitle");
+        this.Input = XUtils.AddElement<HTMLInputElement>(this.HTML, "input", "XTagEditorInput");
+        this.Title.addEventListener('click', () => this.Input.focus(), false);
+        this.Title.innerHTML = "Tag Editor";
+        this.SVG = XUtils.AddElement<HTMLImageElement>(this.HTML, "img", "XTagEditorSVG");
+        this.SVG.src = "svg/closered.svg";
+        this.Input.addEventListener('input', () => this.onInput(this.Input, ""), false)
+    }
+    Input: HTMLInputElement;
+    Title: HTMLSpanElement;
+    SVG: HTMLImageElement;
+
+    private onInput(pInput: HTMLInputElement, pMask?: string)
+    {
+        if (pMask)
+        {
+            const digits = pInput.value.replace(/\D/g, '')
+            pInput.value = this.ApplyMask(digits, pMask)
+        }
+        XUtils.ApplySize(pInput, pInput.value, pInput.style.font);
+    }
+
+    private ApplyMask(val: string, mask: string): string
+    {
+        let out = ''
+        let vi = 0
+        let seenHashes = 0
+
+        for (const m of mask)
+        {
+            if (m === '#')
+            {
+                seenHashes++
+                if (vi < val.length)
+                    out += val[vi++]
+            }
+            else if (val.length > seenHashes)
+                out += m
+        }
+
+        return out
+    }
+}
+
+class XEditableTag extends XDiv 
+{
+    constructor(pOwner: XElement | HTMLElement | null, pClass: string | null = null)
+    {
+        super(pOwner, pClass ?? "XEditableTag");
+        this.TagEditor = new XTagEditor(this, "XTagEditor");
+        this.TagEditor.SVG.addEventListener("click", () => this.DoClick(), false);
+    }
+    TagEditor: XTagEditor;
+    OnClick!: (pTag: XEditableTag) => void;
+    DoClick()
+    {
+        if (this.OnClick != null)
+            this.OnClick.apply(this, [this]);
+    }
+
+    protected override CreateContainer(): HTMLElement 
+    {
+        return XUtils.AddElement<HTMLElement>(null, "div", null);
+    }
+}
